c10db4abe67a93c8cdfcc08f89779e6ef066671a
Hema <hemalathasiriveliram@gmail.com>
2023-08-11 11:09:04 +0530

    lalalala

--- DemoGithub.java
+++ DemoGithub.java
@@ -2,7 +2,6 @@ class DemoGithub
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
 		for(int i=0;i<=10;i++)
 		{
 			System.out.println("yayyy welcome to java class boys and girls!");
